a7399c73a551201eb23e215f1e41e0ab24eeefa9
Famakan Kontaga <kontagafamakan98@gmail.com>
2026-04-12 14:59:27 -0400

    fix: migrate frontend to vite and restore pwa setup

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,11 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
-import * as serviceWorkerRegistration from './serviceWorkerRegistration';
-import { initSentry } from './utils/sentry';
-
-// Initialiser Sentry en premier (si activé)
-initSentry();
+import * as serviceWorkerRegistration from "./serviceWorkerRegistration";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
@@ -15,6 +11,6 @@ root.render(
 
 // Register service worker for offline support
 serviceWorkerRegistration.register({
-  onSuccess: () => console.log('Service Worker: App is cached for offline use'),
-  onUpdate: () => console.log('Service Worker: New content available')
+  onSuccess: () => console.log("Service Worker: App is cached for offline use"),
+  onUpdate: () => console.log("Service Worker: New content available"),
 });
